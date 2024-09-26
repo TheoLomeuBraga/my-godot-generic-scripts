4f571920f4b8261a16809d05d7e5f0e99d79d6cc
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2024-09-26 17:37:46 -0300

    a

--- virtual_Joystick/example/test_joystick.gd
+++ virtual_Joystick/example/test_joystick.gd
@@ -8,4 +8,12 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$Label.text = "X: " + str($VirtualJoystick.get_joystick_input().x) + " Y: " + str($VirtualJoystick.get_joystick_input().y)
+	$Label.text = "X: " + str($VirtualJoystick.get_joystick_input().x) + "\n Y: " + str($VirtualJoystick.get_joystick_input().y)
+
+
+func _on_touch_screen_button_pressed() -> void:
+	print("A")
+
+
+func _on_touch_screen_button_2_pressed() -> void:
+	print("B")
--- virtual_Joystick/virtual_joystick.gd
+++ virtual_Joystick/virtual_joystick.gd
@@ -1,5 +1,27 @@
 extends TouchScreenButton
 
+var center_point : Vector2 = Vector2.ZERO
+@export var range : float = 16.0
+
+func _ready() -> void:
+	if texture_normal != null:
+		center_point = global_position + ((texture_normal.get_size() / 2) * scale)
+	shape = CircleShape2D.new()
+	shape.radius = range
+
+func get_joystick_input() -> Vector2:
+	return $point.position.normalized() * ($point.position / range)
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventScreenDrag:
+		if center_point.distance_to(event.position) < range * (scale.x):
+			$point.global_position = event.position - ((texture_normal.get_size() / 2) * scale)
+		
+
+func _on_released() -> void:
+	$point.position = Vector2.ZERO
+
+'''
 var point_center : Vector2 = Vector2.ZERO
 var max_distance : float = shape.radius
 
@@ -7,16 +29,6 @@ func _ready() -> void:
 	$point.position = point_center
 	$point.visibility_mode = visibility_mode
 	set_process(false)
-
-'''
-func _input(event: InputEvent) -> void:
-	if event is InputEventScreenTouch or event is InputEventMouseButton:
-		if event.pressed:
-			set_process(true)
-		else:
-			set_process(false)
-			$point.position = point_center
-'''
 
 func _process(delta: float) -> void:
 	$point.global_position = get_global_mouse_position()
@@ -33,3 +45,4 @@ func _on_released() -> void:
 
 func get_joystick_input() -> Vector2:
 	return $point.position.normalized() * ($point.position / max_distance)
+'''
