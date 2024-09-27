06ac0957236f60cfb1aecebbbe653e7001089563
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2024-09-27 08:34:37 -0300

    a

--- virtual_Joystick/virtual_joystick.gd
+++ virtual_Joystick/virtual_joystick.gd
@@ -21,29 +21,3 @@ func _input(event: InputEvent) -> void:
 
 func _on_released() -> void:
 	$point.position = Vector2.ZERO
-
-'''
-var point_center : Vector2 = Vector2.ZERO
-var max_distance : float = shape.radius
-
-func _ready() -> void:
-	$point.position = point_center
-	$point.visibility_mode = visibility_mode
-	set_process(false)
-
-func _process(delta: float) -> void:
-	$point.global_position = get_global_mouse_position()
-	$point.position = ($point.position - ($point.texture_normal.get_size() / 2)).limit_length(max_distance)
-
-
-func _on_pressed() -> void:
-	set_process(true)
-
-
-func _on_released() -> void:
-	set_process(false)
-	$point.position = point_center
-
-func get_joystick_input() -> Vector2:
-	return $point.position.normalized() * ($point.position / max_distance)
-'''
