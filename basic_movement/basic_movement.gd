d7ab5182aa14e855155f9eeaec61ba78ff942689
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2024-09-29 18:31:18 -0300

    a

--- basic_movement/basic_movement.gd
+++ basic_movement/basic_movement.gd
@@ -8,21 +8,25 @@ var in_floor : bool = false
 
 @export var speed := 3.0
 
-var has_jump_recently := 0.0
-
 var move_direction_last_frame : Vector3 = Vector3.ZERO
 @export var move_direction : Vector3 = Vector3.ZERO
 
 @export var jump_power := 3.0
 
-@export  var air_control := false
+@export var air_control := false
+
+var juping := false
+@export var gravity_control : bool = false
+@export var gravity_control_curve : Curve
+var juping_progresion := 0.0
 
 func jump() -> void:
-	linear_velocity.y = jump_power
-	has_jump_recently = 0.1
+	if not gravity_control:
+		linear_velocity.y = jump_power
+	juping = true
+	juping_progresion = 0.0
 
 func move(delta) -> void:
-	
 	
 	
 	
@@ -63,17 +67,30 @@ func move(delta) -> void:
 			new_velocity = new_velocity.normalized() * min(new_velocity.length(), speed)
 			
 			linear_velocity.x = new_velocity.x
-			if new_velocity.y < 0 and has_jump_recently <= 0:
+			if new_velocity.y < 0 and not juping:
 				linear_velocity.y = new_velocity.y
 			linear_velocity.z = new_velocity.z
 			
 	if move_direction_last_frame != Vector3.ZERO and move_direction == Vector3.ZERO and not (sliding_time > 0 or not in_floor):
 		linear_velocity = Vector3.ZERO
 	
+	if gravity_control:
+		if juping:
+			gravity_scale = 0
+			juping_progresion += delta
+			linear_velocity.y = gravity_control_curve.sample(juping_progresion) * jump_power
+		else:
+			gravity_scale = 1
+	
+	
+	
+	if linear_velocity.y <=0 and in_floor:
+		juping = false
+
 	if in_floor:
 		sliding_time -= delta
 	
-	has_jump_recently -= delta
+
 	
 	move_direction_last_frame = move_direction
 	in_floor_last_frame = in_floor
