f83c342e5b25d2a1fec4eecaa900e56f676b1449
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2024-09-29 11:34:47 -0300

    a

--- basic_movement/basic_movement.gd
+++ basic_movement/basic_movement.gd
@@ -3,16 +3,21 @@ class_name BasicMovement
 
 var sliding_time := 0.0
 
+var jump_recently := 0.0
+
 var in_floor : bool = false
 
 @export var speed := 3.0
 
 @export var move_direction : Vector3 = Vector3.ZERO
 
-@export var jump_power = 3
+@export var jump_power := 3.0
+
+@export  var air_control := false
 
 func jump() -> void:
 	linear_velocity.y = jump_power
+	jump_recently = 0.2
 
 func move(delta) -> void:
 	
@@ -38,21 +43,30 @@ func move(delta) -> void:
 	if new_move_direction != Vector3.ZERO:
 		new_move_direction = new_move_direction.normalized()
 		if sliding_time > 0 or not in_floor:
-			var new_velocity = new_move_direction * speed * delta
-			new_velocity = new_velocity.normalized() * min(new_velocity.length(), speed)
-			apply_impulse(new_velocity)
-			physics_material_override.friction = 0
+			if air_control:
+				var new_velocity = new_move_direction * speed  * 100 * delta
+				new_velocity = new_velocity.normalized() * min(new_velocity.length(), speed)
+				linear_velocity.x = new_velocity.x
+				linear_velocity.z = new_velocity.z
+				physics_material_override.friction = 0
+			else:
+				var new_velocity = new_move_direction * speed * delta
+				new_velocity = new_velocity.normalized() * min(new_velocity.length(), speed)
+				apply_impulse(new_velocity)
+				physics_material_override.friction = 0
 		else:
 			var new_velocity = new_move_direction * speed  * 100 * delta
 			new_velocity = new_velocity.normalized() * min(new_velocity.length(), speed)
 			linear_velocity.x = new_velocity.x
+			if jump_recently <= 0:
+				linear_velocity.y = new_velocity.y
 			linear_velocity.z = new_velocity.z
 	
 	
 	
 	if in_floor:
 		sliding_time -= delta
-		
+	jump_recently -= delta
 
 func _on_body_entered(body) -> void:
 	if body is RigidBody3D:
@@ -68,3 +82,4 @@ func movement_plugin(delta) -> void:
 func _physics_process(delta) -> void:
 	movement_plugin(delta)
 	move(delta)
+
